da49027e99145077f919b9cdff54f19f58833325
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-17 23:22:17 +0300

    Update bot_1.js

--- bot_1.js
+++ bot_1.js
@@ -1,6 +1,5 @@
-const Discord = require('discord.js');
-
-const Util = require('discord.js');
+onst Discord = require('discord.js');
+const client = new Discord.Client();
 
 const getYoutubeID = require('get-youtube-id');
 
